34806d8fe4a309aa53705e0c3ee954c0e652a0fc
amir991mm <amir991m@gmail.com>
2023-02-19 11:33:11 +0100

    cors1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
-app.use(
+server.use(
   cors({
     origin: "http://localhost:3000",
   })
